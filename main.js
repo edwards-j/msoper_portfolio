60a439d4f315f1c870d310aaeca2772383e75f68
Jonathan Edwards <jonathan.d.edwards92@gmail.com>
2020-02-22 15:42:39 -0600

    adding logo subtext

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ $(window).scroll(function() {
     if (scroll >= 200) {
         $(".logo").removeClass("logo-big");
         $(".logo").addClass("logo-small");
+
+        $(".logo-text").removeClass("show-logo-text")
+        $(".logo-text").addClass("hide-logo-text")
     }
 
     if (scroll <= 200) {
         $(".logo").addClass("logo-big");
         $(".logo").removeClass("logo-small");
+
+        $(".logo-text").addClass("show-logo-text")
+        $(".logo-text").removeClass("hide-logo-text")
     }
 });
